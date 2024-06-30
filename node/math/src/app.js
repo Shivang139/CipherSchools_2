1fdc10459af06faead0e03cd7be12bb768e764f7
shivang139 <shivangagrawal139@gmail.com>
2024-07-01 00:54:35 +0530

    NODE

--- node/math/src/app.js
+++ node/math/src/app.js
@@ -1,10 +1,15 @@
-const http=require("http");
+const express =require("express");
+const app=express();
+app.get("/",(req, res)=>{
+    res.send("this is response from my first node server");
+})
 
-const server=http.createServer((req, res)=>{
-    res.write("this is response from my first node server");
-    res.end();
-});
+app.get("/add",(req,res)=>{
+    let{ a: firstNumber , b: secondNumber }=req.query;
+    let sum=parseInt(firstNumber)+parseInt(secondNumber);
+    res.send({sum});
+})
 
-server.listen(1309,()=>{
+app.listen(1309,()=>{
     console.log(`server is running`);
 })
